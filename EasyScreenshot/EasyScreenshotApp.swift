196c5aaa1f467d78da0e740585d10887963a0098
Barry Martin <bymartin@users.noreply.github.com>
2024-12-05 17:00:22 -0600

    Initial Commit

---
+++ EasyScreenshot/EasyScreenshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyScreenshotApp.swift
+//  EasyScreenshot
+//
+//  Created by Barry Martin on 12/5/24.
+//
+
+import SwiftUI
+
+@main
+struct EasyScreenshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
