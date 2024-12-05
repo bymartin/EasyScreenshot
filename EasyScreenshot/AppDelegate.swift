1db83f98e01e8b9eed8952179fa3a2fb6a00d9ed
Barry Martin <bymartin@users.noreply.github.com>
2024-12-05 17:20:02 -0600

    Starting version - menu bar item created

---
+++ EasyScreenshot/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  EasyScreenshot
+//
+//  Created by Barry Martin on 12/5/24.
+//
+
+import SwiftUI
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+  var statusBarItem: NSStatusItem?
+  func applicationDidFinishLaunching(_ notification: Notification) {
+    
+    statusBarItem = NSStatusBar.system
+      .statusItem(withLength: NSStatusItem.squareLength)
+    if let statusBarButton = statusBarItem?.button {
+      statusBarButton.image = NSImage(
+        systemSymbolName: "cursorarrow.rays",
+        accessibilityDescription: nil
+      )
+    }
+    
+    let mainMenu = NSMenu()
+    
+    statusBarItem?.menu = mainMenu
+  }
+}
+
--- EasyScreenshot/EasyScreenshotApp.swift
+++ EasyScreenshot/EasyScreenshotApp.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 @main
 struct EasyScreenshotApp: App {
+    
+    @NSApplicationDelegateAdaptor(AppDelegate.self)
+    private var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
